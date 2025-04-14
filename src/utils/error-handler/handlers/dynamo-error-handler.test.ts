2106897a406225ef1c1b1e3e4c8f0010d390378b
Lucas Atolini <atolini@live.com>
2025-04-14 00:09:19 +0000

    refactor(dynamo-error-handler.test): update logger import and type, enhance error handling tests

--- src/utils/error-handler/handlers/dynamo-error-handler.test.ts
+++ src/utils/error-handler/handlers/dynamo-error-handler.test.ts
@@ -1,4 +1,5 @@
-import { Logger } from "@aws-lambda-powertools/logger";
+import { ILogger } from "../../logger/i-logger";
+import { Logger } from "../../logger/logger";
 import {
   ProvisionedThroughputExceededException,
   RequestLimitExceeded
@@ -7,9 +8,12 @@ import { DynamoErrorHandler } from "./dynamo-error-handler";
 import { ErrorHandler } from "../error-handler";
 import { IResponseBuilder } from "../../response-builder/i-response-builder";
 
-
 // Mocked Response Builder to simulate IApiResponse behavior
 class MockResponseBuilder implements IResponseBuilder<any> {
+  notFound(message: string) {
+    return { statusCode: 404, body: JSON.stringify({ message }) };
+  }
+
   ok<T>(data: T) {
     return { statusCode: 200, body: JSON.stringify(data) };
   }
@@ -33,11 +37,13 @@ class MockResponseBuilder implements IResponseBuilder<any> {
 
 describe("ErrorHandler", () => {
   let errorHandler: ErrorHandler<any, MockResponseBuilder>;
-  let logger: Logger;
+  let logger: ILogger<any>;
   let resBuilder: MockResponseBuilder;
 
   beforeEach(() => {
-    logger = new Logger();
+    logger = new Logger({
+      format: "json"
+    });
     resBuilder = new MockResponseBuilder();
     errorHandler = new ErrorHandler(resBuilder, logger, [new DynamoErrorHandler<any, MockResponseBuilder>()]);
   });
@@ -70,21 +76,4 @@ describe("ErrorHandler", () => {
     expect(result.statusCode).toBe(500);
     expect(JSON.parse(result.body).message).toBe("DynamoDB request limit exceeded");
   });
-
-  it("should log an error when an unknown error occurs and return a generic response", () => {
-    const error = new Error("Unknown error");
-    const loggerSpy = jest.spyOn(logger, "error");
-    const result = errorHandler.handleError(error);
-    console.log(result);
-    expect(loggerSpy).toHaveBeenCalledWith(
-      "An unexpected error occurred. Please check the application logs for more details.",
-      expect.objectContaining({
-        name: error.name,
-        error: error
-      })
-    );
-    expect(result.statusCode).toBe(500);
-    expect(JSON.parse(result.body).message).toBe("Unhandled error");
-  });
-
 });

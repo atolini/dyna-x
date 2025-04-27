578a602e6403234d1998815ea4268e957e6c1059
Lucas Atolini <atolini@live.com>
2025-04-27 21:33:49 +0000

    refactor: delete docs

--- dist/dyna-x/index.js
+++
@@ -1,5 +0,0 @@
-export { ConditionBuilder } from "./condition-builder";
-export { DynaXRepository } from "./dyna-x-repository";
-export { MaxItemsExceededError } from "./max-item-exceeded-error";
-export { DynaXSchema } from "./dyna-x-schema";
-export { UpdateBuilder } from "./update-builder";

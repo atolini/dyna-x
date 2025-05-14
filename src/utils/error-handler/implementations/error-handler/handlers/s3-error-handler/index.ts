54d6a3b6bec80f7fdf5bd68c3acc07e4178894b0
Lucas Atolini <atolini@live.com>
2025-05-14 02:22:01 +0000

    refactor: enhance S3ErrorHandler to handle additional S3-related errors and improve response mapping

--- src/utils/error-handler/implementations/error-handler/handlers/s3-error-handler/index.ts
+++ src/utils/error-handler/implementations/error-handler/handlers/s3-error-handler/index.ts
@@ -1,47 +1,93 @@
 import { ILogger } from '../../../../../logger/contracts';
 import { IResponseBuilder } from '../../../../../response-builder/contracts';
 import { IErrorActions } from '../../../../contracts/i-error-actions';
-import { S3FileNotFoundError } from '../../../../../../services/storage/implementations/s3/s3-file-not-found-error';
-import { NoSuchBucket } from '@aws-sdk/client-s3';
+import {
+  EncryptionTypeMismatch,
+  InvalidObjectState,
+  InvalidRequest,
+  NoSuchBucket,
+  NoSuchKey,
+} from '@aws-sdk/client-s3';
 
 /**
- * Handles S3-related errors and builds appropriate responses.
- *
+ * @class S3ErrorHandler
+ * @implements {IErrorActions<T, R>}
  * @template T - Response type
  * @template R - Response builder type
+ *
+ * @classdesc
+ * Handles excetions thrown within the {@link S3StorageService} class.
+ *
+ * The following exceptions may be handled:
+ * - **EncryptionTypeMismatch**: The specified encryption type does not match the bucket's default encryption.
+ * - **InvalidRequest**: 	The request is invalid. This can occur for various malformed or unsupported actions.
+ * - **NoSuchKey**: The specified key does not exist in the bucket.
+ * - **NoSuchBucket**: The specified bucket does not exist.
+ * - **InvalidObjectState**: The operation is not valid for the object's storage class.
  */
 export class S3ErrorHandler<T, R extends IResponseBuilder<T>>
   implements IErrorActions<T, R>
 {
   canHandle(error: Error): boolean {
     return (
-      error instanceof S3FileNotFoundError || error instanceof NoSuchBucket
+      error instanceof EncryptionTypeMismatch ||
+      error instanceof InvalidRequest ||
+      error instanceof NoSuchKey ||
+      error instanceof NoSuchBucket ||
+      error instanceof InvalidObjectState
     );
   }
 
   handle(error: Error, logger: ILogger<any>, resBuilder: R): T {
-    const message = 'S3 operation failed';
+    const errorMap = [
+      {
+        type: EncryptionTypeMismatch,
+        log: {},
+        response: () =>
+          resBuilder.badRequest(
+            "The specified encryption type does not match the bucket's default encryption.",
+          ),
+      },
+      {
+        type: InvalidRequest,
+        log: {},
+        response: () =>
+          resBuilder.badRequest(
+            'The request is invalid. Please check the request parameters.',
+          ),
+      },
+      {
+        type: NoSuchKey,
+        log: {},
+        response: () =>
+          resBuilder.notFound(
+            'The specified key does not exist in the bucket.',
+          ),
+      },
+      {
+        type: NoSuchBucket,
+        log: {},
+        response: () =>
+          resBuilder.notFound('The specified bucket does not exist.'),
+      },
+      {
+        type: InvalidObjectState,
+        log: {},
+        response: () =>
+          resBuilder.badRequest(
+            "The operation is not valid for the object's storage class.",
+          ),
+      },
+    ];
 
-    if (error instanceof S3FileNotFoundError) {
-      logger.error({
-        description: `${message}: File not found in S3`,
-        name: error.name,
-        message: error.message,
-        details: `The object with key "${error.key}" was not found in the S3 bucket.`,
-      });
-
-      return resBuilder.notFound(error.message) as T;
-    }
-
-    if (error instanceof NoSuchBucket) {
-      logger.error({
-        description: `${message}: Bucket not found`,
-        name: error.name,
-        message: error.message,
-        details: `The specified S3 bucket may be missing, deleted, or the name is incorrect.`,
-      });
-
-      return resBuilder.internalError('S3 bucket does not exist') as T;
+    for (const entry of errorMap) {
+      if (error instanceof entry.type) {
+        logger.error({
+          name: error.name,
+          message: error.message,
+        });
+        return entry.response() as T;
+      }
     }
   }
 }

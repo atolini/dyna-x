bfb8c16c7644d00b4c4f5a537dc0c58a1f568506
Lucas Atolini <atolini@live.com>
2025-05-21 15:15:30 +0000

    feat: implement CognitoErrorHandler to manage AWS Cognito exceptions

---
+++ src/services/auth/implementations/cognito/cognito-error-handler.ts
@@ -0,0 +1,141 @@
+import { ILogger } from '@logger/contracts/i-logger';
+import { IResponseBuilder } from '@response-builder/contracts';
+import { IErrorActions } from '@error-handler/contracts';
+import {
+  InvalidParameterException,
+  UserNotFoundException,
+  UsernameExistsException,
+  LimitExceededException,
+  InternalErrorException,
+  InvalidPasswordException,
+  NotAuthorizedException,
+  TooManyRequestsException,
+  InvalidEmailRoleAccessPolicyException,
+} from '@aws-sdk/client-cognito-identity-provider';
+
+/**
+ * @class CognitoErrorHandler
+ * @implements {IErrorActions<T, R>}
+ * @template T - Response type
+ * @template R - Response builder type
+ *
+ * @classdesc
+ * Handles exceptions thrown within the {@link CognitoUserService} class.
+ *
+ * The following exceptions may be handled by this class:
+ *
+ * - **InternalErrorException**: If there is an internal error in the AWS Cognito service.
+ * - **InvalidParameterException**: If the provided parameters are invalid.
+ * - **InvalidPasswordException**: If the provided password does not meet the policy requirements.
+ * - **NotAuthorizedException**: If the caller is not authorized to perform the operation.
+ * - **UsernameExistsException**: If the username already exists in the User Pool.
+ * - **TooManyRequestsException**: If the request is throttled due to too many requests.
+ * - **InvalidEmailRoleAccessPolicyException**: If Cognito isn't allowed to use your email identity.
+ * - **UserNotFoundException**: If the specified user does not exist in the User Pool.
+ */
+export class CognitoErrorHandler<T, R extends IResponseBuilder<T>>
+  implements IErrorActions<T, R>
+{
+  /**
+   *
+   */
+  canHandle(error: Error): boolean {
+    return (
+      error instanceof InternalErrorException ||
+      error instanceof InvalidParameterException ||
+      error instanceof InvalidPasswordException ||
+      error instanceof NotAuthorizedException ||
+      error instanceof UsernameExistsException ||
+      error instanceof TooManyRequestsException ||
+      error instanceof InvalidEmailRoleAccessPolicyException ||
+      error instanceof UserNotFoundException
+    );
+  }
+
+  /**
+   *
+   */
+  handle(error: Error, logger: ILogger<any>, resBuilder: R): T {
+    if (error instanceof UserNotFoundException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.notFound(error.message) as T;
+    }
+
+    if (error instanceof InvalidParameterException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.badRequest('Invalid parameter in the request') as T;
+    }
+
+    if (error instanceof InvalidPasswordException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.badRequest('Password does not meet requirements') as T;
+    }
+
+    if (error instanceof NotAuthorizedException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.forbidden(
+        'Not authorized to perform this operation',
+      ) as T;
+    }
+
+    if (error instanceof UsernameExistsException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.badRequest('User already exists') as T;
+    }
+
+    if (error instanceof TooManyRequestsException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.tooManyRequests(
+        'Too many requests - throttling limit reached',
+      ) as T;
+    }
+
+    if (error instanceof LimitExceededException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.tooManyRequests(
+        'Request limit exceeded for Cognito',
+      ) as T;
+    }
+
+    if (error instanceof InvalidEmailRoleAccessPolicyException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.internalError(
+        'Invalid email role access policy configuration',
+      ) as T;
+    }
+
+    if (error instanceof InternalErrorException) {
+      logger.error({
+        name: error.name,
+        message: error.message,
+      });
+      return resBuilder.internalError(
+        'Internal error occurred in Cognito',
+      ) as T;
+    }
+  }
+}

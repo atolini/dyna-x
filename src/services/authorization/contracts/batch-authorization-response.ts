635d17834f889f1e980c85c8210744fbf352ef52
Lucas Atolini <atolini@live.com>
2025-05-23 15:58:01 +0000

    refactor: reorder JSDoc annotations for improved clarity in BatchAuthorizationResponse interface

--- src/services/authorization/contracts/batch-authorization-response.ts
+++ src/services/authorization/contracts/batch-authorization-response.ts
@@ -1,9 +1,9 @@
 import { AuthorizationResponse } from '.';
 
 /**
- * @template R The type that identifies the resource.
- *
  * Represents the result of a batch authorization check, containing the decision for each individual resource.
+ * 
+ * @template R The type that identifies the resource.
  *
  * @property {AuthorizationResponse<R>[]} results - An array of authorization responses, each representing the decision for a specific resource.
  */
